c32e8e763b8db5723ad2dd6991eb62ead7191c29
Briant Fabela <briantfabela@umail.ucsb.edu>
2018-04-19 19:59:28 -0700

    uploaded website
    yay!

---
+++ assets/europe.js
@@ -0,0 +1,98 @@
+var my_cities = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0080ff",
+        "marker-size": "large",
+        "marker-symbol": "bus",
+        "name": "Paris",
+        "country": "France"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2.373046875,
+          48.879167148960214
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ff0000",
+        "marker-size": "medium",
+        "marker-symbol": "rail-metro",
+        "name": "London",
+        "country": "England"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.142822265625,
+          51.52241608253253
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#f2960b",
+        "marker-size": "medium",
+        "marker-symbol": "parking",
+        "name": "Amsterdam",
+        "country": "Netherlands"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          4.85595703125,
+          52.358828590091186
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#800080",
+        "marker-size": "medium",
+        "marker-symbol": "cemetery",
+        "name": "Brussels",
+        "country": "Belgium"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          4.383544921875,
+          50.84757295365389
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#00ff00",
+        "marker-size": "medium",
+        "marker-symbol": "star",
+        "name": "Vienna",
+        "country": "Austria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.3861083984375,
+          48.228332127214934
+        ]
+      }
+    }
+  ]
+}
+
+var heatCities = [
+	[48.879167148960214, 2.373046875],
+	[51.52241608253253, -0.142822265625],
+	[52.358828590091186, 4.85595703125],
+	[50.84757295365389, 4.383544921875],
+	[48.228332127214934, 16.3861083984375]
+];
